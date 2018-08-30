b032925afb84338caad2526ae46a6fc62d79a506
Jeffrey Garcia Wong <jeffrey_garcia_wong@manulife.com>
2018-08-30 12:11:12 +0800

    add secure storage

---
+++ iOS/TestSecureStorage/Pods/Target Support Files/Pods-SecureStorageTests/Pods-SecureStorageTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SecureStorageTests : NSObject
+@end
+@implementation PodsDummy_Pods_SecureStorageTests
+@end
---
+++ iOS/TestSecureStorage/SecureStorage/Test.m
@@ -0,0 +1,9 @@
+//
+//  Test.m
+//  SecureStorage
+//
+//  Created by jeffrey on 21/6/2018.
+//  Copyright © 2018 jeffrey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
